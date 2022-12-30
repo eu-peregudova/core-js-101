d2399b97437c5c423a799343891783c7a6f90950
Eugenia <peregoodovaeva@gmail.com>
2022-12-30 14:27:05 +0200

    feat: dates done

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -12,15 +12,15 @@
  * For rfc2822 date specification refer to : http://tools.ietf.org/html/rfc2822#page-14
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example:
  *    'December 17, 1995 03:24:00'    => Date()
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -28,14 +28,14 @@ function parseDataFromRfc2822(/* value */) {
  * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
  *
  * @param {string} value
- * @return {date}
+ * @return {Date}
  *
  * @example :
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -43,8 +43,8 @@ function parseDataFromIso8601(/* value */) {
  * Returns true if specified date is leap year and false otherwise
  * Please find algorithm here: https://en.wikipedia.org/wiki/Leap_year#Algorithm
  *
- * @param {date} date
- * @return {bool}
+ * @param {Date} date
+ * @return {boolean}
  *
  * @example :
  *    Date(1900,1,1)    => false
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const check = new Date(`${date.getFullYear()}-02-29`);
+  return check.getMonth() === 1;
 }
 
 
@@ -62,8 +63,8 @@ function isLeapYear(/* date */) {
  * Returns the string representation of the timespan between two dates.
  * The format of output string is "HH:mm:ss.sss"
  *
- * @param {date} startDate
- * @param {date} endDate
+ * @param {Date} startDate
+ * @param {Date} endDate
  * @return {string}
  *
  * @example:
@@ -73,8 +74,29 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const diff = endDate - startDate;
+  let mils = diff % 1000;
+  mils = mils.toString();
+  if (mils.length < 3) {
+    mils = '0'.repeat(3 - mils.length) + mils;
+  }
+  let sec = Math.floor(diff / 1000) % 60;
+  sec = sec.toString();
+  if (sec.length < 2) {
+    sec = '0'.repeat(2 - sec.length) + sec;
+  }
+  let mins = Math.floor(diff / 1000 / 60) % 60;
+  mins = mins.toString();
+  if (mins.length < 2) {
+    mins = '0'.repeat(2 - mins.length) + mins;
+  }
+  let hours = Math.floor(diff / 1000 / 60 / 60) % 60;
+  hours = hours.toString();
+  if (hours.length < 2) {
+    hours = '0'.repeat(2 - hours.length) + hours;
+  }
+  return `${hours}:${mins}:${sec}.${mils}`;
 }
 
 
@@ -85,7 +107,7 @@ function timeSpanToString(/* startDate, endDate */) {
  *
  * SMALL TIP: convert to radians just once, before return in order to not lost precision
  *
- * @param {date} date
+ * @param {Date} date
  * @return {number}
  *
  * @example:
@@ -94,8 +116,16 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const dateNew = new Date(date);
+  const hour = dateNew.getUTCHours() % 12;
+  const minutes = dateNew.getUTCMinutes();
+  let angle = 0.5 * (60 * hour - 11 * minutes);
+  if (angle > 180) {
+    angle = 360 - angle;
+  }
+  const degreesToRadians = (deg) => (deg * Math.PI) / 180.0;
+  return Math.sqrt(degreesToRadians(angle) ** 2);
 }
 
 
